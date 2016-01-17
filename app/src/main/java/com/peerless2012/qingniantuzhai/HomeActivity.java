53ef7c073c4478af16e7175e303e6989df95f111
peerless2012 <peerless2012@126.com>
2016-01-18 01:16:14 +0800

    初步测试网络以及retrofit和okhttp，但是遇到一个操蛋的问题

--- app/src/main/java/com/peerless2012/qingniantuzhai/HomeActivity.java
+++ app/src/main/java/com/peerless2012/qingniantuzhai/HomeActivity.java
@@ -3,6 +3,7 @@ package com.peerless2012.qingniantuzhai;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.peerless2012.qingniantuzhai.net.NetWorkService;
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,9 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        findViewById(R.id.get_info).setOnClickListener(this);
     }
 
     @Override
@@ -97,5 +109,52 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private String baseUrl = "http://192.168.31.213:8080";
+    @Override
+    public void onClick(View v) {
+//        OkHttpClient client = new OkHttpClient();
+//        Retrofit retrofit = new Retrofit.Builder()
+//                        .client(client)
+//                        .baseUrl(baseUrl)
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+//
+//        NetWorkService rottenTomatoesService = retrofit.create(NetWorkService.class);
+//        Call<UserInfo> call = rottenTomatoesService.getUserInfo();
+//        call.enqueue(new Callback<UserInfo>() {
+//            @Override
+//            public void onResponse(Response<UserInfo> response) {
+////                Toast.makeText(HomeActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+////                Toast.makeText(HomeActivity.this,"出错了!",Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                OkHttpClient client = new OkHttpClient();
+                Retrofit retrofit = new Retrofit.Builder()
+                                .client(client)
+                                .baseUrl(baseUrl)
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .build();
+
+                NetWorkService rottenTomatoesService = retrofit.create(NetWorkService.class);
+                Call<UserInfo> call = rottenTomatoesService.getUserInfo();
+                        try {
+                            Response<UserInfo> execute = call.execute();
+                            Log.i("Home",execute.body().toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+        }.start();
     }
 }
